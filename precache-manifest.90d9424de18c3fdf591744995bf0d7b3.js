8dd8e25b0a909e3f1f1e1774cc6a3b4f0aa05d75
nathan-pc <nn5g14@soton.ac.uk>
2020-07-02 21:23:11 +0100

    Updates

---
+++ precache-manifest.90d9424de18c3fdf591744995bf0d7b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03078497761c5d4ca9da318dcbc88e81",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "76f4c69165e8feb505a4",
+    "url": "/weather-app/static/css/main.3822b27c.chunk.css"
+  },
+  {
+    "revision": "74c098bb6fd430482d63",
+    "url": "/weather-app/static/js/2.479ef69c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weather-app/static/js/2.479ef69c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76f4c69165e8feb505a4",
+    "url": "/weather-app/static/js/main.cd59a8de.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
